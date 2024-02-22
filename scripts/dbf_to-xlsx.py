00a79bafcc7d6180360bd3059de143b1e5e1aaf8
wkndavid <workingnown@gmail.com>
2024-02-22 11:19:30 -0300

    [update]

--- scripts/dbf_to-xlsx.py
+++ scripts/dbf_to-xlsx.py
@@ -2,7 +2,7 @@ import pandas as pd
 from dbfread import DBF
 
 # Caminho para o arquivo DBF
-caminho_dbf = '/var/www/html/version2/files/teste/teste.dbf'
+caminho_dbf = '/var/www/html/python2.7_translater_files_geoprocessing/files/teste/teste.dbf'
 
 # Ler o arquivo DBF usando pandas
 table = DBF(caminho_dbf, encoding='utf-8')
@@ -10,7 +10,7 @@ table = DBF(caminho_dbf, encoding='utf-8')
 df = pd.DataFrame(list(table))
 
 # Caminho para salvar o arquivo XLSX
-caminho_xlsx = '/var/www/html/version2/files/resultados/results_dbf_xlsx/results_dbf_xlsx.xlsx'
+caminho_xlsx = '/var/www/html/python2.7_translater_files_geoprocessing/files/resultados/results_dbf_xlsx/results_dbf_xlsx.xlsx'
 
 # Salvar o DataFrame como XLSX usando pandas
 df.to_excel(caminho_xlsx, index=False, engine='openpyxl')
